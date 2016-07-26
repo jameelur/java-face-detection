f65bf16b4ef54abf832f8929e91a782e7aa82077
jyw-x230 <jinyiwu1909@gmail.com>
2016-07-26 21:18:36 +0800

    Changed similarity computation

--- src/com/cs/comp7502/SimilarityComputation.java
+++ src/com/cs/comp7502/SimilarityComputation.java
@@ -17,10 +17,10 @@ public class SimilarityComputation {
         super();
     }
 
-    public static boolean cosSimilarity(List<Integer> vector1, List<Integer> vector2, double threshold) {
+    public static double cosSimilarity(List<Integer> vector1, List<Integer> vector2, double threshold) {
         if (vector1.size() != vector2.size()) {
             System.out.println("Two vectors have different length");
-            return false;
+            return 0.0;
         }
 
         int innerDot = 0;
@@ -32,18 +32,20 @@ public class SimilarityComputation {
             normSquare2 += vector2.get(i) * vector2.get(i);
         }
 
-        double similarity = Math.abs((double) innerDot / (Math.sqrt(normSquare1) * Math.sqrt(normSquare2)));
-
+        if ((Math.sqrt(normSquare1) * Math.sqrt(normSquare2)) == 0.0) {
+            return 0.0;
+        } else {
+            double similarity = Math.abs((double) innerDot / (Math.sqrt(normSquare1) * Math.sqrt(normSquare2)));
 //        System.out.println("similarity: " + similarity);
-
-        return similarity >= threshold;
+            return similarity;
+        }
 
     }
 
-    public static boolean correlation(List<Integer> vector1, List<Integer> vector2, double threshold) {
+    public static double correlation(List<Integer> vector1, List<Integer> vector2, double threshold) {
         if (vector1.size() != vector2.size()) {
             System.out.println("Two vectors have different length");
-            return false;
+            return 0.0;
         }
 
         int length = vector1.size();
@@ -59,15 +61,18 @@ public class SimilarityComputation {
             squareSum2 += vector2.get(i) * vector2.get(i);
         }
 
-        double rho = Math.abs((double) (length * innerDot - sum1 * sum2) / (Math.sqrt(length * squareSum1 - sum1 * sum1) * Math.sqrt(length * squareSum2 - sum2 * sum2)));
-
-        return rho >= threshold;
+        if ((Math.sqrt(length * squareSum1 - sum1 * sum1) * Math.sqrt(length * squareSum2 - sum2 * sum2)) == 0) {
+            return 0.0;
+        } else {
+            double rho = Math.abs((double) (length * innerDot - sum1 * sum2) / (Math.sqrt(length * squareSum1 - sum1 * sum1) * Math.sqrt(length * squareSum2 - sum2 * sum2)));
+            return rho;
+        }
 
     }
 
     public static double voting(PrintWriter writer, WeakHaarClassifier queryClassifier, List<WeakHaarClassifier> referenceClassifiers, double threshold) {
         int numOfRefFace = referenceClassifiers.size();
-        int numOfPosVote = 0;
+        double sumOfSimilarity = 0.0;
 
 
         List<HaarFeature> queryFeatuerList = queryClassifier.getClassifierList();
@@ -75,27 +80,111 @@ public class SimilarityComputation {
 
         for (int featureTypeIndex = 0; featureTypeIndex < numOfFeatuerType; ++featureTypeIndex) {
             List<Integer> queryFeatureVector = queryFeatuerList.get(featureTypeIndex).getFeatureVector();
-            int newPosVote = 0;
+            double newAddedSimilarity = 0.0;
 
             for (WeakHaarClassifier weakHaarClassifier : referenceClassifiers) {
                 List<Integer> referenceFeatureVector = weakHaarClassifier.getClassifierList().get(featureTypeIndex).getFeatureVector();
-                if (cosSimilarity(queryFeatureVector, referenceFeatureVector, threshold))
-                    ++newPosVote;
-//                if (correlation(queryFeatureVector, referenceFeatureVector, threshold))
-//                    ++newPosVote;
+                newAddedSimilarity += cosSimilarity(queryFeatureVector, referenceFeatureVector, threshold);
+//                newAddedSimilarity += correlation(queryFeatureVector, referenceFeatureVector, threshold);
             }
 
 //            System.out.print("Positive Vote for feature " + featureTypeIndex + ": " + newPosVote + " / " + numOfRefFace);
 //            System.out.print(featureTypeIndex + ": " + newPosVote + " / " + numOfRefFace);
-            writer.print(featureTypeIndex + ": " + newPosVote + " / " + numOfRefFace + "_ ");
-            numOfPosVote += newPosVote;
+            writer.print(featureTypeIndex + ": " + newAddedSimilarity / numOfRefFace);
+            sumOfSimilarity += newAddedSimilarity;
         }
 
-        double rate = (double) numOfPosVote / (numOfRefFace * numOfFeatuerType);
+        double average = sumOfSimilarity / (numOfRefFace * numOfFeatuerType);
 
 //        System.out.println("\n The overall positive voting rate: " + rate);
-        writer.println("The overall positive voting rate: " + rate);
+        writer.println("The average similarity: " + average);
 
-        return rate;
+        return average;
     }
+
+//    public static boolean cosSimilarity(List<Integer> vector1, List<Integer> vector2, double threshold) {
+//        if (vector1.size() != vector2.size()) {
+//            System.out.println("Two vectors have different length");
+//            return false;
+//        }
+//
+//        int innerDot = 0;
+//        int normSquare1 = 0, normSquare2 = 0;
+//
+//        for (int i = 0; i < vector1.size(); ++i) {
+//            innerDot += vector1.get(i) * vector2.get(i);
+//            normSquare1 += vector1.get(i) * vector1.get(i);
+//            normSquare2 += vector2.get(i) * vector2.get(i);
+//        }
+//
+//
+//        if ((Math.sqrt(normSquare1) * Math.sqrt(normSquare2)) == 0.0) {
+//            return true;
+//        } else {
+//            double similarity = Math.abs((double) innerDot / (Math.sqrt(normSquare1) * Math.sqrt(normSquare2)));
+////        System.out.println("similarity: " + similarity);
+//            return similarity >= threshold;
+//        }
+//    }
+//
+//    public static boolean correlation(List<Integer> vector1, List<Integer> vector2, double threshold) {
+//        if (vector1.size() != vector2.size()) {
+//            System.out.println("Two vectors have different length");
+//            return false;
+//        }
+//
+//        int length = vector1.size();
+//        int innerDot = 0;
+//        int sum1 = 0, sum2 = 0;
+//        int squareSum1 = 0, squareSum2 = 0;
+//
+//        for (int i = 0; i < length; ++i) {
+//            innerDot += vector1.get(i) * vector2.get(i);
+//            sum1 += vector1.get(i);
+//            sum2 += vector2.get(i);
+//            squareSum1 += vector1.get(i) * vector1.get(i);
+//            squareSum2 += vector2.get(i) * vector2.get(i);
+//        }
+//
+//        if ((Math.sqrt(length * squareSum1 - sum1 * sum1) * Math.sqrt(length * squareSum2 - sum2 * sum2)) == 0) {
+//            return true;
+//        } else {
+//            double rho = Math.abs((double) (length * innerDot - sum1 * sum2) / (Math.sqrt(length * squareSum1 - sum1 * sum1) * Math.sqrt(length * squareSum2 - sum2 * sum2)));
+//            return rho >= threshold;
+//        }
+//    }
+//
+//    public static double voting(PrintWriter writer, WeakHaarClassifier queryClassifier, List<WeakHaarClassifier> referenceClassifiers, double threshold) {
+//        int numOfRefFace = referenceClassifiers.size();
+//        int numOfPosVote = 0;
+//
+//
+//        List<HaarFeature> queryFeatuerList = queryClassifier.getClassifierList();
+//        int numOfFeatuerType = queryFeatuerList.size();
+//
+//        for (int featureTypeIndex = 0; featureTypeIndex < numOfFeatuerType; ++featureTypeIndex) {
+//            List<Integer> queryFeatureVector = queryFeatuerList.get(featureTypeIndex).getFeatureVector();
+//            int newPosVote = 0;
+//
+//            for (WeakHaarClassifier weakHaarClassifier : referenceClassifiers) {
+//                List<Integer> referenceFeatureVector = weakHaarClassifier.getClassifierList().get(featureTypeIndex).getFeatureVector();
+//                if (cosSimilarity(queryFeatureVector, referenceFeatureVector, threshold))
+//                    ++newPosVote;
+////                if (correlation(queryFeatureVector, referenceFeatureVector, threshold))
+////                    ++newPosVote;
+//            }
+//
+////            System.out.print("Positive Vote for feature " + featureTypeIndex + ": " + newPosVote + " / " + numOfRefFace);
+////            System.out.print(featureTypeIndex + ": " + newPosVote + " / " + numOfRefFace);
+//            writer.print(featureTypeIndex + ": " + newPosVote + " / " + numOfRefFace + "_ ");
+//            numOfPosVote += newPosVote;
+//        }
+//
+//        double rate = (double) numOfPosVote / (numOfRefFace * numOfFeatuerType);
+//
+////        System.out.println("\n The overall positive voting rate: " + rate);
+//        writer.println("The overall positive voting rate: " + rate);
+//
+//        return rate;
+//    }
 }
